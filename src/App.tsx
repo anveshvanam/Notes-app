db9cc94b1082c17097bf3b7343b74d0a7aab8da8
anvesh <anveshvanam5@gamail.com>
2023-06-04 16:30:27 +0530

    set up NoteForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
 import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { NewNote } from "./NewNote";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type RawNote = {
+  id: string;
+};
+
+export type RawNoteDat = {
+  title: string;
+  markdown: string;
+  tagsIds: string[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
 function App() {
+  const [notes, setNotes] = useLocalStorage<Note[]>("notes", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
   return (
     <Routes>
       <Route path="/" element={<h1>Hi</h1>} />
